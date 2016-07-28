9252c1c663be54386ea9a9fcf37a0e8762a65fc8
cleavs <cleavon98@yahoo.com>
2016-07-28 13:09:28 -0700

    variables

---
+++ age.rb
@@ -0,0 +1,15 @@
+puts 'How old are you?'
+age = gets.chomp
+myage = age.to_i + 10
+twen_age = age.to_i + 20
+thir_age = age.to_i + 30
+fort_age = age.to_i + 40
+
+puts 'In 10 years you will be:' 
+puts myage
+puts 'In 20 years you will be:'
+puts twen_age
+puts 'In 30 years you will be:'
+puts thir_age
+puts 'In 40 years you will be:'
+puts fort_age
