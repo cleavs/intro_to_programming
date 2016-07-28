0b3ae4749a0195fe0692b35279de8cc3d46742bc
cleavs <cleavon98@yahoo.com>
2016-07-28 12:03:32 -0700

    First chpater-strings

---
+++ strings.rb
@@ -0,0 +1,35 @@
+puts "cleavon" + " "+"brathwaite"
+
+thousand = 5467 /1000
+puts thousand
+hundred = 5467 % 1000 / 100
+puts hundred
+ten = 5467 % 100 / 10
+puts ten
+one = 5467 % 10
+puts one
+
+movies = {shawshank_redemption:'1975',
+			god_father: '1994',
+			grease:'1970',
+			jaws:'1974'}
+
+puts movies [:shawshank_redemption]
+puts movies [:god_father]
+puts movies [:grease]
+puts movies [:jaws]
+
+movies = ['1975','1994','1970','1974']
+puts movies[0]
+puts movies[1]
+puts movies[2]
+puts movies[3]
+
+number5= puts 5*4*3*2*1
+number6= puts 6*5*4*3*2*1
+number7= puts 7*6*5*4*3*2*1
+number8= puts 8*7*6*5*4*3*2*1
+
+puts 6.7 * 6.7
+puts 2.4 * 2.4
+puts 1.7 * 1.7
